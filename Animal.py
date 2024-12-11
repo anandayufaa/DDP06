0187b6cc2d690a32f148973366dbf0a20e8c36d3
anandayufaa <anandayupa@gmail.com>
2024-12-11 16:25:38 +0700

    Add files via upload

---
+++ Animal.py
@@ -0,0 +1,15 @@
+class Animal:
+    def __init__(self, nama, makanan, hidup, berkembang_biak):
+        self.nama = nama
+        self.makanan = makanan
+        self.hidup = hidup
+        self.berkembang_biak = berkembang_biak
+    def cetak(self):
+        print("Nama \t\t: ", self.nama,
+              "\makanan \t\t: ", self.makanan,
+              "\hidup \t\t: ", self.hidup,
+              "\berkembang_biak \t\t: ", self.berkembang_biak
+              )
+    
+object = Animal("buaya", "Daging", "Amphibi", "bertelur")
+print(object.cetak())
